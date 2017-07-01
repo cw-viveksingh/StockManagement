c8d204a9fc66bfa52270f17baa2b77a562613688
Vivek Singh Tomar <vivek.tomar@aepl.com>
2017-07-01 17:14:37 +0530

    Paging Implementation

--- UsedStockManagement/Controllers/UsedStockController.cs
+++ UsedStockManagement/Controllers/UsedStockController.cs
@@ -18,25 +18,48 @@ namespace UsedStockManagement.Controllers
         private QueryDescriptor<UsedCarStock> query = new QueryDescriptor<UsedCarStock>();
         
         // GET: UsedStock
-        public ActionResult Index(int? page, string city = null, int minBudget = 0, int maxBudget = int.MaxValue)
+        public ActionResult Index()
         {
             try
             {
-                queryContainer = query.Term(p => p.IsDeleted, 0);
-
-                if (city != null)
-                    queryContainer &= query.Term(p => p.City, city);
-
-                if (minBudget != 0 || maxBudget != int.MaxValue)
-                    queryContainer &= query.Range(r => r.OnField(fi => fi.Price).LowerOrEquals(maxBudget).GreaterOrEquals(minBudget));
-                                   
+                queryContainer = query.Term(p => p.IsDeleted, 0);                                   
                 var searchResults = elasticClient.Search<UsedCarStock>(s => s
+                    .From(0)
+                    .Size(5)
                     .Index("usedstock")
                     .Type("usedcarstock")
                     .Query(queryContainer)
                 );
                 
-                return View("~/Views/Home/UsedStockSearch.cshtml", searchResults.Documents.ToList().ToPagedList(page ?? 1, 2));
+                return View("~/Views/Home/UsedStockSearch.cshtml", searchResults.Documents);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //GET: Filter Results
+        public ActionResult FilterSearch(int page, string city = null, int minBudget = 0, int maxBudget = int.MaxValue)
+        {
+            queryContainer = query.Term(p => p.IsDeleted, 0);
+            if (city != null)
+                queryContainer &= query.Term(p => p.City, city);
+
+            if (minBudget != 0 || maxBudget != int.MaxValue)
+                queryContainer &= query.Range(r => r.OnField(fi => fi.Price).LowerOrEquals(maxBudget).GreaterOrEquals(minBudget));
+
+            try
+            {
+                var searchResults = elasticClient.Search<UsedCarStock>(s => s
+                    .From((page-1) * (5 - 1))
+                    .Size(5)
+                    .Index("usedstock")
+                    .Type("usedcarstock")
+                    .Query(queryContainer)
+                );
+
+                return View("~/Views/Shared/_bindCarResult.cshtml", searchResults.Documents);
             }
             catch (Exception ex)
             {
--- UsedStockManagement/Scripts/script.js
+++ UsedStockManagement/Scripts/script.js
@@ -1,29 +1,61 @@
 ﻿$(document).ready(function () {
-    $('#filterSearch').click(function () {
-        
-        var filterURL = "/UsedStock/Index/?";
-        var city = $("select#location option:checked").val();
-        var minBudget = $("select#minBudget option:checked").val();
-        var maxBudget = $("select#maxBudget option:checked").val();
-        var flag = 0;
+    $('#previousResult').prop('disabled', true);
+    $('#nextResult').prop('disabled', false);
 
+    var city = $("select#location option:checked").val();
+    var minBudget = $("select#minBudget option:checked").val();
+    var maxBudget = $("select#maxBudget option:checked").val();
+    var page = 1;
+
+    function loadData(filterURL) {
         if (city != "all") {
-            filterURL += "city=" + city;
-            if (minBudget != 0 || maxBudget != 2147483647) {
-                filterURL += "&minBudget=" + minBudget;
-                filterURL += "&maxBudget=" + maxBudget;
-            }
-        } else if (minBudget != 0 || maxBudget != 2147483647) {
-            filterURL += "minBudget=" + minBudget;
+            filterURL += "&city=" + city;
+        }
+
+        if (minBudget != 0 || maxBudget != 2147483647) {
+            filterURL += "&minBudget=" + minBudget;
             filterURL += "&maxBudget=" + maxBudget;
         }
-        window.location.href = filterURL;
+
+        $("#listingStock").load(filterURL, function (response, status) {
+            if (status == "error") {
+                console.log("error occured");
+            }
+        });
+
+    }
+
+    $('#previousResult').click(function () {
+        page -= 1;
+        if ($('#nextResult').is(':disabled')) {
+            $('#nextResult').prop('disabled', false);
+        }
+        if (page == 1) {
+            $('#previousResult').prop('disabled', true);
+        }
+        loadData("/UsedStock/FilterSearch/?page=" + page);
     });
-    $('.search').click(function() {
+
+    $('#nextResult').click(function () {
+        page += 1;
+        $('#previousResult').prop('disabled', false);
+        loadData("/UsedStock/FilterSearch/?page=" + page);
+    });
+
+    $('#filterSearch').click(function () {
+        city = $("select#location option:checked").val();
+        minBudget = $("select#minBudget option:checked").val();
+        maxBudget = $("select#maxBudget option:checked").val();
+        page = 1;
+        $('#previousResult').prop('disabled', true);
+        $('#nextResult').prop('disabled', false);
+        loadData("/UsedStock/FilterSearch/?page=" + page);
+    });
+
+    $('.search').click(function () {
         var id = $(this).parent().find(".car_id").text();
         window.location.href = "/StockDetail/Info/" + id;
-        }
+    }
      )
 
 });
-
